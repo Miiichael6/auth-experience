a33261e0d449cbf84281973eaaf67a7f952d261b
Michael Canales <CDLSMichael.16@gmail.com>
2022-12-27 15:49:34 -0500

    variables de entorno

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const app = require("./src/app");
 const { sequelize } = require("./src/config/database");
 
-const port = process.env || 4000;
+const port = process.env.PORT || 4000;
+
+console.log(process.env.PORT)
+
 app.listen(port, async () => {
   await sequelize.sync({ force: false });
   console.log(`server is listen the port http://localhost:4000`);
--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const mainRoutes = require("./routes/index.Routes");
 const cors = require("cors");
-
+const { FRONT_END_URL } = process.env;
 require("./config/database.js");
 
 const app = express();
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", FRONT_END_URL);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
