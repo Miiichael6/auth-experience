0b6715f7bb69a172a590b34d890c786e870c966c
Michael Canales <CDLSMichael.16@gmail.com>
2022-12-30 18:17:23 -0500

    abc

--- backend/src/middleware/checkAuth.js
+++ backend/src/middleware/checkAuth.js
@@ -2,16 +2,20 @@ const jwt = require("jsonwebtoken");
 const { User } = require("../config/database");
 // verificar usuario
 const checkAuth = async (req, res, next) => {
-  const { myToken } = req.cookies;
-  console.log(myToken);
+  let { myToken } = req.cookies;
+  console.log(myToken, "**** token req. cookies");
 
   if (!myToken) {
     const error = new Error("token no valido");
     return res.status(401).send({ msg: error.message });
   }
+  req.headers.authorization = myToken;
+
+  let tokenAccess = req.headers.authorization;
+  console.log(tokenAccess, "token res.authtorization****");
 
   try {
-    const decoded = jwt.verify(myToken, process.env.JWT_SECRET);
+    const decoded = jwt.verify(tokenAccess, process.env.JWT_SECRET);
     req.logedUser = await User.findOne({
       attributes: ["id", "name", "email", "photo"],
       where: { id: decoded.id },
